d6f74d6bfc3708695cc2cad41364fbb375f59c6c
Yuriy Semevsky <semevskiy@gmail.com>
2021-01-28 20:22:35 +0300

    improved

--- lipsum/lipsum.go
+++ lipsum/lipsum.go
@@ -6,7 +6,12 @@ package lipsum
 
 import "strings"
 
-const source = "Lorem ipsum dolor sit amet est eu velit eligendi moderatius ne vis vitae nonumy deserunt Agam mucius nominavi vim ut at erroribus maluisset  referrentur vis Has reque vulputate consequuntur in ex ius vivendo oporteat  torquatos Ne soluta definitionem vim consul integre erroribus ne est Choro  vitae invidunt eu mel populo expetenda efficiantur ut nec veri nullam  senserit id vim"
+const source = `Lorem ipsum dolor sit amet est eu velit eligendi moderatius ne
+ vis vitae nonumy deserunt Agam mucius nominavi vim ut at erroribus maluisset
+ referrentur vis Has reque vulputate consequuntur in ex ius vivendo oporteat
+ torquatos Ne soluta definitionem vim consul integre erroribus ne est Choro
+ vitae invidunt eu mel populo expetenda efficiantur ut nec veri nullam senserit
+ id vim`
 var array = strings.Fields(source)
 
 func Lipsum(count int) string {
