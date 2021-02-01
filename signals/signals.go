8334f9f31ff649f2d0d7a65f45297956842d2704
Yuriy Semevsky <semevskiy@gmail.com>
2021-02-01 15:42:19 +0300

    reformatted copyright notice

--- signals/signals.go
+++ signals/signals.go
@@ -1,5 +1,4 @@
-// Copyright (c) 2021, Yuriy Semevsky <semevskiy@gmail.com>
-// All rights reserved.
+// Copyright (c) 2021, Yuriy Semevsky <semevskiy@gmail.com>. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file. 
 package signals
