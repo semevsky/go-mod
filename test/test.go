17b358a478562a8a2fa4811f6e3e461d2b44f9a1
Yuriy Semevsky <semevskiy@gmail.com>
2021-02-02 22:55:04 +0300

    added copyright notice

--- test/test.go
+++ test/test.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2021, Yuriy Semevsky <semevskiy@gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file. 
 package main
 
 import (
